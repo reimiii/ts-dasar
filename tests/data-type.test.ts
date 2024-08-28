10277fcc7d7af2396ac053f1005e19dc87bce1d7
Hilmi AM <imiia75775@proton.me>
2024-08-28 12:49:16 +0700

    primitive data type

---
+++ tests/data-type.test.ts
@@ -0,0 +1,11 @@
+describe('data type', () => {
+    it('should return data type primitive', () => {
+       const name: string = "Mee";
+       const balance: number = 1000000;
+       const isGood: boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isGood);
+    });
+});
