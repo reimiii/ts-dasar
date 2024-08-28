52025461702f2b601627b9a7a7ebdfaa57b09085
Hilmi AM <imiia75775@proton.me>
2024-08-28 13:33:54 +0700

    union type using it

--- tests/union.test.ts
+++ tests/union.test.ts
@@ -12,4 +12,20 @@ describe('Union Type', () => {
 
         // sample = []; // error in tsc
     });
+
+    it('should using typeof for union', () => {
+        function process(v: string | number | boolean): any {
+           if (typeof v === "string") {
+               return v.toUpperCase();
+           } else if (typeof v === "number") {
+               return v + 10;
+           } else {
+               return !v;
+           }
+        }
+
+        expect(process("aku suka aku suka")).toBe("AKU SUKA AKU SUKA");
+        expect(process(10)).toBe(20);
+        expect(process(true)).toBe(false);
+    });
 });
