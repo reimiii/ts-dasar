a9a63a8ba42a60f87add6f1e48bc3606354cfd5e
Hilmi AM <imiia75775@proton.me>
2024-08-28 13:08:19 +0700

    array

---
+++ tests/array.test.ts
@@ -0,0 +1,9 @@
+describe('Array', () => {
+    it('should return arrays', () => {
+        const names: string[] = ["hilmi", "arip", "thq"];
+        const ages: number[] = [1, 2, 3];
+
+        console.info(names)
+        console.info(ages)
+    });
+});
