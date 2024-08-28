0bb3af84a17acf0abf32077ef285f2f73da5797f
Hilmi AM <imiia75775@proton.me>
2024-08-28 13:14:45 +0700

    readonly array

--- tests/array.test.ts
+++ tests/array.test.ts
@@ -6,4 +6,11 @@ describe('Array', () => {
         console.info(names)
         console.info(ages)
     });
+
+    it('should readonly array', () => {
+        const theName: ReadonlyArray<string> = ["makan", "minum", "hmm"];
+        console.info(theName)
+
+        // theName[0] = "ugh"; // error in tsc
+    });
 });
