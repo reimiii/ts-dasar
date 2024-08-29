89b8e1802175bbb562086fe951f51da4853eaf1e
Hilmi AM <imiia75775@proton.me>
2024-08-29 13:38:02 +0700

    switch in TS

--- tests/if.test.ts
+++ tests/if.test.ts
@@ -15,4 +15,39 @@ describe('If Statement', () => {
         const say: string = (n >= 70) ? "congrats" : "try again";
         console.info(say)
     });
+
+    it('should switch operator', () => {
+        function getDayName(dayNumber: number): string {
+            let dayName: string;
+            switch (dayNumber) {
+                case 1:
+                    dayName = "Sunday";
+                    break;
+                case 2:
+                    dayName = "Monday";
+                    break;
+                case 3:
+                    dayName = "Tuesday";
+                    break;
+                case 4:
+                    dayName = "Wednesday";
+                    break;
+                case 5:
+                    dayName = "Thursday";
+                    break;
+                case 6:
+                    dayName = "Friday";
+                    break;
+                case 7:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid day";
+                    break;
+            }
+            return dayName;
+        }
+
+        console.info(getDayName(2))
+    });
 });
